b50e0c4c2d0698cc6989c4cc661fc40fd3d30a84
Theodore Chandra <Theodore Gautama Chandra>
2023-12-05 15:23:55 +0700

    fix: setup get coordinates function

--- day3/day3Tools/tools.go
+++ day3/day3Tools/tools.go
@@ -1,12 +1,15 @@
 package day3Tools
 
 import (
+	"fmt"
+
 	"github.com/theo123490/advent-of-code-2023/commonTools"
 )
 
-type Coordinate struct {
-	X int
-	Y int
+type Item struct {
+	Item string
+	X    []int
+	Y    int
 }
 
 func GetFinalResult(inputFile string) int {
@@ -14,16 +17,39 @@ func GetFinalResult(inputFile string) int {
 	return 0
 }
 
-func getSchematicCoordinates(inputFile string) map[string]Coordinate {
+func getSchematicCoordinates(inputFile string) []Item {
 	scanner, file := commonTools.FileReader(inputFile)
 	defer file.Close()
+	var itemSlice []Item = make([]Item, 0)
 	var currentY = 0
 	for scanner.Scan() {
 		var inputString string = scanner.Text()
+
+		var itemRunes []rune = make([]rune, 0)
+		var isObject bool = false
+		var item Item = Item{}
+
 		for currentX, value := range inputString {
-			println("TODO")
+			if value == '.' {
+				if isObject {
+					item.Item = string(itemRunes)
+					itemSlice = append(itemSlice, item)
+
+					itemRunes = make([]rune, 0)
+					isObject = false
+					item = Item{}
+				}
+			} else {
+				isObject = true
+
+				item.X = append(item.X, currentX)
+				item.Y = currentY
+
+				itemRunes = append(itemRunes, value)
+			}
 		}
 		currentY += 1
 	}
-	return make(map[string]Coordinate)
+	fmt.Println(itemSlice)
+	return itemSlice
 }
