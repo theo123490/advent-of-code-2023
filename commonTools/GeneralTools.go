65d731a097bebd6786165c632c962f3a1f4e8173
Theodore Chandra <Theodore Gautama Chandra>
2023-12-19 13:04:30 +0700

    feat: add bubblesort on general tools for no reason

--- commonTools/GeneralTools.go
+++ commonTools/GeneralTools.go
@@ -5,3 +5,26 @@ func PrintErr(err error) {
 		panic(err)
 	}
 }
+
+func BubbleSort(intSlice []int) []int {
+	var ResultSlice []int = make([]int, len(intSlice))
+	for len(intSlice) > 0 {
+		var firstElement int
+		var secondElement int
+		for i := range intSlice {
+			if i+1 >= len(intSlice) {
+				continue
+			}
+			firstElement = intSlice[i]
+			secondElement = intSlice[i+1]
+			if intSlice[i] > intSlice[i+1] {
+				intSlice[i] = secondElement
+				intSlice[i+1] = firstElement
+			}
+		}
+
+		ResultSlice[len(intSlice)-1] = intSlice[len(intSlice)-1]
+		intSlice = intSlice[:len(intSlice)-1]
+	}
+	return ResultSlice
+}
--- day7/day7Tools/tools.go
+++ day7/day7Tools/tools.go
@@ -1,6 +1,15 @@
 package day7Tools
 
+import (
+	"fmt"
+
+	"github.com/theo123490/advent-of-code-2023/commonTools"
+)
+
 func GetFinalResult(inputFile string) int {
+	var testSlice []int = []int{1, 3, 2, 323, 4, 6, 8, 3, 7, 8, 645, 3643, 4}
+	var result []int = commonTools.BubbleSort(testSlice)
+	fmt.Println(result)
 	return 0
 }
 
