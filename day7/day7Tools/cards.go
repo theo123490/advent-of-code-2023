2927a15872b8cf71ba3b496e44a0ab0ba1aad3d6
Theodore Chandra <Theodore Gautama Chandra>
2023-12-20 13:02:53 +0700

    day7: create isHigher function with first card comparison

--- day7/day7Tools/cards.go
+++ day7/day7Tools/cards.go
@@ -1,5 +1,49 @@
 package day7Tools
 
-type Card struct {
-	symbol string
+import (
+	"fmt"
+
+	"github.com/theo123490/advent-of-code-2023/commonTools"
+)
+
+type Card rune
+
+type CardHand struct {
+	cards        []Card
+	cardValueMap map[Card]int
+}
+
+func CreateCardValueMap() map[Card]int {
+	var cardValueMap map[Card]int = map[Card]int{
+		Card("A"[0]): 12,
+		Card("K"[0]): 11,
+		Card("Q"[0]): 10,
+		Card("J"[0]): 9,
+		Card("T"[0]): 8,
+		Card("9"[0]): 7,
+		Card("8"[0]): 6,
+		Card("7"[0]): 5,
+		Card("6"[0]): 4,
+		Card("5"[0]): 3,
+		Card("4"[0]): 2,
+		Card("3"[0]): 1,
+		Card("2"[0]): 0,
+	}
+	return cardValueMap
+}
+
+func (ch CardHand) IsHigher(otherValue commonTools.Valuables) bool {
+	var otherCh CardHand
+	var ok bool
+
+	if otherCh, ok = otherValue.(CardHand); !ok {
+		fmt.Errorf("Can only compare with other cardHands not")
+	}
+
+	if &ch.cardValueMap != &otherCh.cardValueMap {
+		fmt.Errorf("cardValueMap doesnt match")
+	}
+	var chMap map[Card]int = ch.cardValueMap
+
+	return chMap[ch.cards[0]] > chMap[otherCh.cards[0]]
 }
--- day7/day7Tools/tools.go
+++ day7/day7Tools/tools.go
@@ -1,22 +1,30 @@
 package day7Tools
 
-import (
-	"fmt"
-
-	"github.com/theo123490/advent-of-code-2023/commonTools"
-)
+import "fmt"
 
 type myInt int
 
-func (i myInt) IsHigher(otherValue commonTools.Valuables) bool {
-	ii := otherValue.(myInt)
-	return i > ii
-}
-
 func GetFinalResult(inputFile string) int {
-	var testSlice []commonTools.Valuables = []commonTools.Valuables{myInt(1), myInt(3), myInt(2), myInt(323), myInt(4), myInt(6), myInt(8), myInt(3), myInt(7), myInt(8), myInt(645), myInt(3643), myInt(4)}
-	var result []commonTools.Valuables = commonTools.BubbleSort(testSlice)
-	fmt.Println(result)
+	var cardValueMap map[Card]int = CreateCardValueMap()
+	var cardHandA CardHand = CardHand{
+		[]Card{
+			Card("2"[0]),
+			Card("2"[0]),
+			Card("4"[0]),
+			Card("2"[0]),
+			Card("2"[0]),
+		}, cardValueMap}
+
+	var cardHandB CardHand = CardHand{
+		[]Card{
+			Card("4"[0]),
+			Card("2"[0]),
+			Card("4"[0]),
+			Card("2"[0]),
+			Card("2"[0]),
+		}, cardValueMap}
+
+	fmt.Println(cardHandB.IsHigher(cardHandA))
 	return 0
 }
 
